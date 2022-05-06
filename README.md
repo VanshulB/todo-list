e8e52f3a9711f7707f59196f5f97ab3d7fb75347
VanshulB <vanshulbhatia@gmail.com>
2022-05-06 22:43:14 +0530

    Added some final changes

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-Basic todo-listv1 project 
+Basic To-Do List Project Made using HTML, CSS, Express, Node
+
+To use it 
+Pre-requisites - need to have NODE installed on your machine
+
+Open your your teminal (make sure you are in the todo-list directory)
+
+run the following on your terminal
+```node app.js```
+
+open any browser and type localhost:3000
+your todo list web-application will be opened 
+
+it also has a work list
+access it using
+```localhost:3000/work```
+
+you can edit the initial todo list via editing 
+the items array is app.js
 
--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("/public", express.static("public"));
 
+let workItems = [];
+
 app.get("/", function(req, res){
     // res.send("<h1>Hello world</h1>");
     var today = new Date();
@@ -24,18 +26,26 @@ app.get("/", function(req, res){
     // console.log(day);
     
     // res.send("Hello World");
-    res.render("list", {kindOfDay: day, items: items});
+    res.render("list", {listTitle: day, items: items});
 
 });
 
 app.post("/", function(req, res){
+    console.log(req.body);
     item = req.body.newItem;
     items.push(item);
     res.redirect("/");
-    
-
 });
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", items: workItems});
+});
+
+app.post("/work", function(req, res){
+    let item =  req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
 
 app.listen(3000, function (){
     console.log("Server is running on port 3000");
